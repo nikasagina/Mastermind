4fe28fcf6cbfb7fe5cc306fd6d4eeac953671150
nikasagina <nsagi21@freeuni.edu.ge>
2023-05-31 01:49:40 +0400

    Remove unnecessary characters

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ def start_text
   puts 'Hi, Welcome to Mastermind'
   puts 'Please enter your choice'
   puts '1. Code Maker: - you make a code and computer guesses it'
-  puts '2. Code Breaker: - computer randomly selects a code and you try to break it.\n'
+  puts '2. Code Breaker: - computer randomly selects a code and you try to break it.'
   puts
   puts 'Clues:'
   puts '🟠: - Right Number Wrong Position'
@@ -14,10 +14,10 @@ def start_text
 end
 
 def read_input
-  input = gets.chomp!
+  input = gets.chomp
   until input == '1' || input == '2'
     puts 'Invalid input, enter 1 for code maker, 2 for code breaker: '
-    input = gets.chomp!
+    input = gets.chomp
   end
   input
 end
@@ -27,11 +27,10 @@ def play
   start_text
   mode = read_input.to_i
   game = Mastermind.new(mode)
-  mode == 1 ? game.computer_action : game.human_action
+  mode == 1 ? game.computer_mode : game.human_mode
   puts 'play again? (y/n)'
-  input = gets.chomp!.downcase
+  input = gets.chomp.downcase
   self.play if input == 'y'
 end
 
-
-play()
+play
